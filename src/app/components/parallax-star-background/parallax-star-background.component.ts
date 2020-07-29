a7baf6ed32eadc4c9d308759631fc296e79cae7e
Jimmy Verschoor <jimmyverschoor@outlook.com>
2020-07-29 22:18:28 +0200

    header added

--- src/app/components/parallax-star-background/parallax-star-background.component.ts
+++ src/app/components/parallax-star-background/parallax-star-background.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parallax-star-background',
   templateUrl: './parallax-star-background.component.html',
-  styleUrls: ['./parallax-star-background.component.css']
+  styleUrls: ['./parallax-star-background.component.sass']
 })
 export class ParallaxStarBackgroundComponent implements OnInit {
 
